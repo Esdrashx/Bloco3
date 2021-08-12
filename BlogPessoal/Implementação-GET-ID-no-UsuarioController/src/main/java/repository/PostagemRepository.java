9c175fba4e6b0bc9af2620a7ec342fe1f7340643
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-08-11 21:49:58 -0300

    Add files via upload

---
+++ BlogPessoal/Implementação-GET-ID-no-UsuarioController/src/main/java/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blog.repository;
+
+import java.util.List;
+
+import org.generation.blog.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem,Long>{
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+}
---
+++ BlogPessoal/Implementação-Put-de-Usuario/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TemaComponent } from './tema/tema.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch: 'full'},
+
+  {path:'login', component:LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'tema', component: TemaComponent},
+  {path:'tema-edit/:id', component: TemaEditComponent},
+  {path:'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-edit/:id', component: PostagemEditComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+  {path: 'user-edit/:id', component: UserEditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
